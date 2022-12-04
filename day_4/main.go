21ca6dc6ff4175126ace8a0ef4ae8980fb2cbd84
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-12-04 11:41:28 +0100

    Day 4

---
+++ day_4/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func one(data []string) int {
+	var output int
+
+	for _, e := range data {
+		ass := strings.Split(e, ",")
+		elfOne, elfTwo := strings.Split(ass[0], "-"), strings.Split(ass[1], "-")
+
+		elfOne_low, _ := strconv.Atoi(elfOne[0])
+		elfOne_high, _ := strconv.Atoi(elfOne[1])
+		elfTwo_low, _ := strconv.Atoi(elfTwo[0])
+		elfTwo_high, _ := strconv.Atoi(elfTwo[1])
+
+		if elfOne_low <= elfTwo_low && elfOne_high >= elfTwo_high {
+			output += 1
+		} else if elfTwo_low <= elfOne_low && elfTwo_high >= elfOne_high {
+			output += 1
+		}
+	}
+
+	return output
+}
+
+func two(data []string) int {
+	var output int
+
+	for _, e := range data {
+		ass := strings.Split(e, ",")
+		elfOne, elfTwo := strings.Split(ass[0], "-"), strings.Split(ass[1], "-")
+
+		elfOne_low, _ := strconv.Atoi(elfOne[0])
+		elfOne_high, _ := strconv.Atoi(elfOne[1])
+		elfTwo_low, _ := strconv.Atoi(elfTwo[0])
+		elfTwo_high, _ := strconv.Atoi(elfTwo[1])
+
+		if elfTwo_low <= elfOne_low && elfOne_low <= elfTwo_high {
+			output += 1
+		} else if elfTwo_low <= elfOne_high && elfOne_high <= elfTwo_high {
+			output += 1
+		} else if elfOne_low <= elfTwo_low && elfTwo_low <= elfOne_high {
+			output += 1
+		} else if elfOne_low <= elfTwo_high && elfTwo_high <= elfOne_high {
+			output += 1
+		}
+	}
+
+	return output
+}
+
+func readData(path string) []string {
+	var data []string
+	readFile, err := os.Open(path)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		val := fileScanner.Text()
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		data = append(data, val)
+	}
+
+	return data
+}
+
+func main() {
+	data := readData("input.txt")
+	fmt.Println("Part one:", one(data))
+	fmt.Println("Part two:", two(data))
+}
