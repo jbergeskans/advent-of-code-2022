8f56696890a03c652bc8660f47d7157a0a82801b
Jesper Bergeskans <jesperbergeskans@devoteam.com>
2022-12-11 16:22:05 +0100

    Part 2............

--- day_11/main.go
+++ day_11/main.go
@@ -59,10 +59,63 @@ func one(monkeys []monkey) int {
 	return one * two
 }
 
-func genMonkeys(data []string) []monkey {
+func two(monkeys []monkey, end int, sm int) int {
+	// for _, e := range monkeys {
+	// 	fmt.Println(e.id, e.inspectCount, e.currItems)
+	// }
+	for i := 0; i < end; i++ {
+		// fmt.Println(monkeys)
+		for idx := range monkeys {
+			// fmt.Println("Running for", idx, "Svals:", monkeys[idx].currItems)
+			// fmt.Println(idx, len(monkeys[idx].currItems))
+			for _, item := range monkeys[idx].currItems {
+				var worryLevel int
+				op := strings.Split(monkeys[idx].operation, " ")
+				val, _ := strconv.Atoi(op[1])
+
+				monkeys[idx].inspectCount++
+
+				if op[0] == "mul" {
+					if val == 0 {
+						// fmt.Println(idx, val)
+						worryLevel = item * item
+					} else {
+						worryLevel = item * val
+					}
+				} else if op[0] == "add" {
+					worryLevel = item + val
+				}
+
+				// fmt.Println(idx, worryLevel, worryLevel%monkeys[idx].divBy, monkeys[idx].passTrue, monkeys[idx].passFalse)
+				if worryLevel%monkeys[idx].divBy == 0 {
+					monkeys[monkeys[idx].passTrue].currItems = append(monkeys[monkeys[idx].passTrue].currItems, worryLevel%sm)
+					// fmt.Println("==", monkeys[idx].passTrue, monkeys[monkeys[idx].passTrue].currItems)
+				} else {
+					monkeys[monkeys[idx].passFalse].currItems = append(monkeys[monkeys[idx].passFalse].currItems, worryLevel%sm)
+					// fmt.Println("==", monkeys[idx].passFalse, monkeys[monkeys[idx].passFalse].currItems)
+				}
+			}
+			monkeys[idx].currItems = nil
+		}
+	}
+
+	var one, two int
+	for _, mnk := range monkeys {
+		if mnk.inspectCount > one {
+			two = one
+			one = mnk.inspectCount
+		} else if mnk.inspectCount > two && mnk.inspectCount < one {
+			two = mnk.inspectCount
+		}
+	}
+	return one * two
+}
+
+func genMonkeys(data []string) ([]monkey, int) {
 	var monkeyStats [][]string
 	var tmp []string
 	var monkeyList []monkey
+	sm := 1
 
 	for _, e := range data {
 		if e == "" {
@@ -96,6 +149,7 @@ func genMonkeys(data []string) []monkey {
 
 		inTest := strings.Split(input[3], " ")
 		mnkTest, _ := strconv.Atoi(inTest[len(inTest)-1])
+		sm *= mnkTest
 
 		inTrue := strings.Split(input[4], " ")
 		mnkTrue, _ := strconv.Atoi(inTrue[len(inTrue)-1])
@@ -107,12 +161,14 @@ func genMonkeys(data []string) []monkey {
 		monkeyList = append(monkeyList, newMonkey)
 	}
 
-	return monkeyList
+	return monkeyList, sm
 }
 
 func main() {
 	// data := readinput.ReadData("input_test.txt")
 	data := readinput.ReadData("input.txt")
-	monkeys := genMonkeys(data)
+	monkeys, _ := genMonkeys(data)
 	fmt.Println("Part one:", one(monkeys))
+	monkeys, sm := genMonkeys(data)
+	fmt.Println("Part two:", two(monkeys, 10000, sm))
 }
